57272a3d7db6ce402b0fee5ead170ccc411d8d28
mdtanker <matt.d.tankersley@gmail.com>
2025-04-02 17:17:49 -0400

    add package logger

--- src/airbornegeo/__init__.py
+++ src/airbornegeo/__init__.py
@@ -9,3 +9,11 @@ from __future__ import annotations
 from ._version import version as __version__
 
 __all__ = ["__version__"]
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+logger.addHandler(logging.NullHandler())
