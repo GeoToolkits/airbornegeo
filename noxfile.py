90e952e52e6945da3072cc61428bfebd092f45e3
mdtanker <matt.d.tankersley@gmail.com>
2026-03-14 15:35:58 +0100

    replace nox doc commands with pixi tasks

--- noxfile.py
+++ noxfile.py
@@ -1,46 +1,12 @@
-import argparse
 import shutil
 from pathlib import Path
 
 import nox
 
 DIR = Path(__file__).parent.resolve()
-PROJECT = nox.project.load_toml()
 
 nox.needs_version = ">=2025.2.9"
 nox.options.default_venv_backend = "uv|virtualenv"
-
-
-@nox.session(venv_backend="mamba", python="3.12", reuse_venv=True, default=False)
-def docs(session: nox.Session) -> None:
-    """
-    Build the docs. Pass --non-interactive to avoid serving. First positional argument is the target directory.
-    """
-    session.conda_install("geopandas", "pygmt")
-    doc_deps = nox.project.dependency_groups(PROJECT, "docs")
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-b", dest="builder", default="html", help="Build target (default: html)"
-    )
-    parser.add_argument("output", nargs="?", help="Output directory")
-    args, posargs = parser.parse_known_args(session.posargs)
-    serve = args.builder == "html" and session.interactive
-
-    session.install("-e.", *doc_deps, "sphinx-autobuild")
-
-    shared_args = (
-        "-n",  # nitpicky mode
-        "-T",  # full tracebacks
-        f"-b={args.builder}",
-        "docs",
-        args.output or f"docs/_build/{args.builder}",
-        *posargs,
-    )
-
-    if serve:
-        session.run("sphinx-autobuild", "--open-browser", *shared_args)
-    else:
-        session.run("sphinx-build", "--keep-going", *shared_args)
 
 
 @nox.session
